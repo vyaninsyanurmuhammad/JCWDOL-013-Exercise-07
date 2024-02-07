08d9c013ee854766bd664800508a62d2eabbd5f6
vyaninsyanurmuhammad <vyaninsya.nurmuhammad@gmail.com>
2024-02-07 13:55:38 +0700

    feat: Create a function to get the intersection of two objects

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ console.log(checkTwoObjectsEqual({ a: "Hello" }, { a: 1 }));
 console.log(checkTwoObjectsEqual({ a: 1 }, { a: 1 }));
 
 console.log("-------------------------------------------------------");
+
+// Create a function to get the intersection of two objects
+
+const getIntersectionTwoEqual = (objA, objB) => {
+    let result = {};
+
+    for (const key in objA) {
+
+        if (objB.hasOwnProperty(key) && objA[key] === objB[key]) {
+            result[key] = objA[key];
+        }
+    }
+
+    return result;
+}
+
+console.log(getIntersectionTwoEqual({ a: 1, b: 2 }, { a: 1, c: 3 }));
+
+console.log("-------------------------------------------------------");
